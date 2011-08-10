8f1ae627bd569afa70a2123e4d0f13d615b36663
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-08-10 20:34:28 +0200

    register validator using EPackage literals instead of using the registry.
    this is a workaround for
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=354425

--- org.eclipse.emf.ecore.xcore/src/org/eclipse/emf/ecore/xcore/validation/XcoreJavaValidator.java
+++ org.eclipse.emf.ecore.xcore/src/org/eclipse/emf/ecore/xcore/validation/XcoreJavaValidator.java
@@ -1,13 +1,34 @@
 package org.eclipse.emf.ecore.xcore.validation;
- 
 
-public class XcoreJavaValidator extends AbstractXcoreJavaValidator {
+import java.util.ArrayList;
+import java.util.List;
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.xcore.XcorePackage;
+import org.eclipse.xtext.common.types.TypesPackage;
+import org.eclipse.xtext.xbase.XbasePackage;
+import org.eclipse.xtext.xtype.XtypePackage;
+
+public class XcoreJavaValidator extends AbstractXcoreJavaValidator
+{
+
+	@Override
+	protected List<EPackage> getEPackages()
+	{
+		List<EPackage> result = new ArrayList<EPackage>();
+		result.add(XcorePackage.eINSTANCE);
+		result.add(XbasePackage.eINSTANCE);
+		result.add(TypesPackage.eINSTANCE);
+		result.add(XtypePackage.eINSTANCE);
+		return result;
+	}
+
+	// @Check
+	// public void checkGreetingStartsWithCapital(Greeting greeting) {
+	// if (!Character.isUpperCase(greeting.getName().charAt(0))) {
+	// warning("Name should start with a capital",
+	// MyDslPackage.Literals.GREETING__NAME);
+	// }
+	// }
 
 }
